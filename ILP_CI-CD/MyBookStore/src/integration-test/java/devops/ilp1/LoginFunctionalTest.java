7cdcaeec25e633c617d8c36b91c712944ab7dea3
JyotiS2507 <69745968+JyotiS2507@users.noreply.github.com>
2020-08-16 23:17:42 +0530

    Update LoginFunctionalTest.java

--- ILP_CI-CD/MyBookStore/src/integration-test/java/devops/ilp1/LoginFunctionalTest.java
+++ ILP_CI-CD/MyBookStore/src/integration-test/java/devops/ilp1/LoginFunctionalTest.java
@@ -39,13 +39,14 @@ public class LoginFunctionalTest {
         driver = new FirefoxDriver(firefoxOptions);*/
 	System.setProperty("webdriver.chrome.driver","/home/yetirajam/Desktop/work/jyoti/chromedriver"); 
 		ChromeOptions options = new ChromeOptions();
-options.addArguments("start-maximized"); // open Browser in maximized mode
+/*options.addArguments("start-maximized"); // open Browser in maximized mode
 options.addArguments("disable-infobars"); // disabling infobars
 options.addArguments("--disable-extensions"); // disabling extensions
 options.addArguments("--disable-gpu"); // applicable to windows os only
 options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
 options.addArguments("--no-sandbox"); // Bypass OS security model
-		options.addArguments("--headless"); 
+		options.addArguments("--headless"); */
+		options.setExperimentalOption("useAutomationExtension", false);
 	driver = new ChromeDriver(options);  
 	}
 
