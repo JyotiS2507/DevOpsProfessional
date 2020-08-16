ce317013fccefe6cab2a3c81a2fcac920872557e
JyotiS2507 <69745968+JyotiS2507@users.noreply.github.com>
2020-08-16 23:14:44 +0530

    Update LoginFunctionalTest.java

--- ILP_CI-CD/MyBookStore/src/integration-test/java/devops/ilp1/LoginFunctionalTest.java
+++ ILP_CI-CD/MyBookStore/src/integration-test/java/devops/ilp1/LoginFunctionalTest.java
@@ -45,6 +45,7 @@ options.addArguments("--disable-extensions"); // disabling extensions
 options.addArguments("--disable-gpu"); // applicable to windows os only
 options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
 options.addArguments("--no-sandbox"); // Bypass OS security model
+		options.addArguments("--headless"); 
 	driver = new ChromeDriver(options);  
 	}
 
